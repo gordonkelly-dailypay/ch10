a0b1d6d87e9f88702ebab8a5df882db001473c0a
Gordon Kelly <gordon.kelly@dailypay.com>
2026-01-09 15:59:08 +0000

    second commit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/learning-go-gk/ch10
+module github.com/gordonkelly-dailypay/ch10
 
 go 1.25.5
--- main.go
+++ main.go
@@ -1,9 +1,8 @@
-package main
+// Package utilities does what you think it does
+package utilities
 
+// Add does what you think it does
+// [Adding docs]: https://www.mathsisfun.com/numbers/addition.html
 func Add(i1 int, i2 int) int {
 	return i1 + i2
-}
-
-func main() {
-
 }
