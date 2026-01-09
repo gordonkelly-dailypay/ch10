968ac63171f49bdc2bee8b347c682beb1eedc47f
Gordon Kelly <gordon.kelly@dailypay.com>
2026-01-09 16:08:58 +0000

    final commit

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 // Package utilities does what you think it does
 package utilities
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add does what you think it does
 // [Adding docs]: https://www.mathsisfun.com/numbers/addition.html
-func Add(i1 int, i2 int) int {
+func Add[T Number](i1 T, i2 T) T {
 	return i1 + i2
 }
